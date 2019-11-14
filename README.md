c32d74a4543e8f1670979ea363e7aec763332233
Collias <jocomail1@yahoo.com>
2019-11-14 10:09:56 -0500

    completed sections 9-12

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is the starter tutorial for JS
+completed 1-8
+completed 9-12
+completed 13-19
--- main.js
+++ main.js
@@ -53,7 +53,7 @@
 
   //string concatenation
   let temp = 20.5;
-  let strcat = "Weather of India is" + temp + "\u00b0c";//u00b0c display degree symbol
+  let strcat = "Weather of Las Vegas is" + temp + "\u00b0c";//u00b0c display degree symbol
   console.log(strcat);
   //use string literal on 2 lines
   let rt = "One line \n Second line";
@@ -158,6 +158,113 @@
     console.log(k+","+p);
   }
   console.log("\n");
+  
+  /*for in loop 
+  for(variable in object)
+    statement;*/
+  const objs={a:1,b:2,c:3,d:4};
+  for(let o in objs){
+    console.log(objs[o]);//object property values on console using keys
+  }     
+  /*variable o=a
+    console.log()=1*/
+  const objs1={a:1,b:2,c:3,d:4};
+  for(let o in objs1){
+    console.log(o);//here only keys show in console
+  }  
+  /*for of loop
+  for(variable of object)
+    statement;*/
+  //declare array
+  let ary=[1,2,3,4,5];  
+  for(let val of ary){
+    val +=1;
+    console.log(val);//show array elements on console; if you used for in loop here you would see the index #s of array 0-4
+  }
+  //declare string
+  let strg="orbit";
+  for(let val of strg){
+    console.log(val);//shows string character on console
+  } 
+
+  /*Simple literals
+    5.3 is a number literal
+    "Daily" is a string literal
+    JS reserve words
+      null, true, false (keywords)
+    Bare expressions
+      str expression evaluate to value of the str 
+      undefined is a global variable not a keyword like null*/
+
+  let y,z;//declare variable y and z
+  y=4*5;//combine 2 expressions with multiplication(*) and assignment operator(=) 
+  z=y=4*5;//combine 3 expressions
+  z=y=20//evaluate multiplication
+  z=20//assign 20 value to variable y //z still undefined
+  20;//assign 20 variable value to variable z
+  console.log(y);//shows variable on console 
+  console.log(z);//shows variable on console
+  /*4,5 is operand,(*) is operator
+    identifier expression and literal expression are not operator expressions
+    everything that results in value is an expression*/
+
+  //pre and post-increment
+  let e,f=5;
+  let g,h=7;
+  let q,t=42;
+  e=++f;//pre increment
+  console.log(e+" with pre-increment");
+  g=h++;//post increment
+  console.log(g+" with post increment");
+  g=h++;//post increment
+  console.log(g+" with post increment");
+  q=--t;//pre decrement
+  console.log(q+" with pre decrement");
+
+  //Comparison Operator
+  let w=71,
+    v=72;
+  console.log(w<v);//true 71<72
+  console.log(w>v);//false 71>72
+  console.log(w<=v);//true 71<=72
+  console.log(w>=v);//false 71>=72
+  
+  //Strict and Abstract Equality
+  let n=100,std='100';//1st variable is 2nd variable is string
+  console.log(n==std);//true n&std have same value does not check type (abstract)
+  console.log(n===std);//false n&std not equal because different types
+    //convert std to # using strict equality
+  console.log(n===Number(std));//convert to #
+    //use not strict equality
+  console.log(n!==Number(std));
+    //use not abstract
+  console.log(n!=std);
+  
+  //And Operator (&&)
+  let xy=10,yz=10;
+  if(xy===10 && yz===10){
+    console.log("true");
+  }else{
+    console.log("false");
+  }
+  //Or Operator (||)
+  let xx=10,yy=10
+  if(xx===10 || yy===5){
+    console.log("true");
+  }else{
+    console.log("false");
+  }//still returns true because 1 of expressions is true
+  
+  //Not operator (!)
+  let zz=true;
+  console.log(zz);
+  console.log(!zz);
+
+    
+    
+
+    
+   
 
       
 
